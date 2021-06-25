abfb478df63945527727e906bb9992900d814b71
Roy Triesscheijn <roy-t@hotmail.com>
2021-06-25 22:59:53 +0200

    Prepare generating code

---
+++ src/Generators/Mini.Engine.Content.Generators/SourceGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Mini.Engine.Content.Generators
+{
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var text = "namespace Mini.Engine.Content { public static class Foo { public static void Bar() {} } }";
+            context.AddSource("Foo.cs", text);
+        }
+    }
+}
---
+++ src/Generators/Mini.Engine.Generators.Debugger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mini.Engine.Generators.Debugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Use View -> Other Windows -> Shader Syntax Visualizer to help create the right code!!");
+            Console.ReadLine();
+        }
+    }
+}
