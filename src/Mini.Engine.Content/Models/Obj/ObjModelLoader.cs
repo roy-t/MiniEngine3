03ed8c8af20cf43048eb981886ff0711970f05cd
Roy Triesscheijn <roy-t@hotmail.com>
2021-11-30 22:45:01 +0100

    Parse mtl files for textures

--- src/Mini.Engine.Content/Models/Obj/ObjModelLoader.cs
+++ src/Mini.Engine.Content/Models/Obj/ObjModelLoader.cs
@@ -35,7 +35,7 @@ public sealed class ObjModelLoader : IModelLoader
                 new ObjectParser(),
                 new CommentParser(),
 
-                new MtlLibParser(logger),
+                new MtlLibParser(),
                 new UseMtlParser()
         };
     }
