40d774a701b0742463be96a1dcc523b3dacfcd67
Roy Triesscheijn <roy-t@hotmail.com>
2022-02-07 20:24:37 +0100

    Move loading to frame

--- src/Mini.Engine/GameLoop.cs
+++ src/Mini.Engine/GameLoop.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Numerics;
-using Mini.Engine.Configuration;
+﻿using Mini.Engine.Configuration;
 using Mini.Engine.Content;
 using Mini.Engine.Controllers;
 using Mini.Engine.DirectX;
-using Mini.Engine.ECS.Components;
-using Mini.Engine.ECS.Entities;
 using Mini.Engine.ECS.Pipeline;
 using Mini.Engine.Graphics;
-using Mini.Engine.Graphics.Lighting.ImageBasedLights;
-using Mini.Engine.Graphics.Lighting.PointLights;
-using Mini.Engine.Graphics.Models;
-using Mini.Engine.Graphics.Transforms;
+using Mini.Engine.Scenes;
 
 namespace Mini.Engine;
 
@@ -25,7 +18,7 @@ internal sealed class GameLoop : IGameLoop
     private readonly CameraController CameraController;
     private readonly ContentManager Content;
     private readonly ParallelPipeline Pipeline;    
-    public GameLoop(Device device, RenderHelper helper, FrameService frameService, CameraController cameraController, RenderPipelineBuilder builder, ContentManager content, EntityAdministrator entities, ComponentAdministrator components, CubeMapGenerator cubeMapGenerator)
+    public GameLoop(Device device, RenderHelper helper, FrameService frameService, CameraController cameraController, RenderPipelineBuilder builder, ContentManager content, SponzaScene scene)
     {
         this.Device = device;
         this.Helper = helper;
@@ -36,42 +29,7 @@ internal sealed class GameLoop : IGameLoop
         content.Push("RenderPipeline");
         this.Pipeline = builder.Build();
 
-        SetScene(content, entities, components, cubeMapGenerator);
-        
-    }
-
-    private static void SetScene(ContentManager content, EntityAdministrator entities, ComponentAdministrator components, CubeMapGenerator cubeMapGenerator)
-    {
-        content.Push("Scene");
-        // TODO: move to scene
-        var entity = entities.Create();
-        //components.Add(new ModelComponent(entity, content.LoadCube()));
-        //components.Add(new TransformComponent(entity).SetScale(1.0f));
-        components.Add(new ModelComponent(entity, content.LoadSponza()));
-        components.Add(new TransformComponent(entity).SetScale(0.01f));
-        //models.Add(new ModelComponent(entity, content.LoadAsteroid()));
-
-
-        var sphere = entities.Create();
-        //models.Add(new ModelComponent(sphere, SphereGenerator.Generate(device, 3, content.LoadDefaultMaterial(), "Sphere")));
-        components.Add(new PointLightComponent(sphere, Vector4.One, 100.0f));
-        components.Add(new TransformComponent(sphere));//.ApplyTranslation(Vector3.One * 10));
-
-
-
-        var sky = entities.Create();
-        //var texture = content.LoadTexture(@"Skyboxes\industrial.hdr");
-        var texture = content.LoadTexture(@"Skyboxes\circus.hdr");
-        var albedo = cubeMapGenerator.GenerateAlbedo(texture, false, "skybox_albedo");
-        var irradiance = cubeMapGenerator.GenerateIrradiance(texture, "skybox_irradiance");
-        var environment = cubeMapGenerator.GenerateEnvironment(texture, "skybox_environment");
-
-        // Make sure the items are disposed whenever this content frame is
-        content.Link(albedo, albedo.Name);
-        content.Link(irradiance, irradiance.Name);
-        content.Link(environment, environment.Name);
-
-        components.Add(new SkyboxComponent(sky, albedo, irradiance, environment, 1.0f));
+        scene.Load();
     }
 
     public void Update(float time, float elapsed)
---
+++ src/Mini.Engine/Scenes/SponzaScene.cs
@@ -0,0 +1,66 @@
+﻿using System.Numerics;
+using Mini.Engine.Configuration;
+using Mini.Engine.Content;
+using Mini.Engine.ECS.Components;
+using Mini.Engine.ECS.Entities;
+using Mini.Engine.Graphics.Lighting.ImageBasedLights;
+using Mini.Engine.Graphics.Lighting.PointLights;
+using Mini.Engine.Graphics.Models;
+using Mini.Engine.Graphics.Transforms;
+
+namespace Mini.Engine.Scenes;
+
+[Service]
+public sealed class SponzaScene
+{
+    private readonly ContentManager Content;
+    private readonly EntityAdministrator Entities;
+    private readonly ComponentAdministrator Components;
+    private readonly CubeMapGenerator CubeMapGenerator;
+
+    public SponzaScene(ContentManager content, EntityAdministrator entities, ComponentAdministrator components, CubeMapGenerator cubeMapGenerator)
+    {
+        this.Content = content;
+        this.Entities = entities;
+        this.Components = components;
+        this.CubeMapGenerator = cubeMapGenerator;
+    }
+
+    public string Title => "Sponza";
+
+    public void Load()
+    {
+        this.Content.Push("Scene");
+
+        var entity = this.Entities.Create();
+        //components.Add(new ModelComponent(entity, content.LoadCube()));
+        //components.Add(new TransformComponent(entity).SetScale(1.0f));
+        this.Components.Add(new ModelComponent(entity, this.Content.LoadSponza()));
+        this.Components.Add(new TransformComponent(entity).SetScale(0.01f));
+        //models.Add(new ModelComponent(entity, content.LoadAsteroid()));
+
+        var sphere = this.Entities.Create();
+        //models.Add(new ModelComponent(sphere, SphereGenerator.Generate(device, 3, content.LoadDefaultMaterial(), "Sphere")));
+        this.Components.Add(new PointLightComponent(sphere, Vector4.One, 100.0f));
+        this.Components.Add(new TransformComponent(sphere));//.ApplyTranslation(Vector3.One * 10));
+
+        var sky = this.Entities.Create();
+        //var texture = content.LoadTexture(@"Skyboxes\industrial.hdr");
+        var texture = this.Content.LoadTexture(@"Skyboxes\circus.hdr");
+        var albedo = this.CubeMapGenerator.GenerateAlbedo(texture, false, "skybox_albedo");
+        var irradiance = this.CubeMapGenerator.GenerateIrradiance(texture, "skybox_irradiance");
+        var environment = this.CubeMapGenerator.GenerateEnvironment(texture, "skybox_environment");
+
+        // Make sure the items are disposed whenever this content frame is
+        this.Content.Link(albedo, albedo.Name);
+        this.Content.Link(irradiance, irradiance.Name);
+        this.Content.Link(environment, environment.Name);
+
+        this.Components.Add(new SkyboxComponent(sky, albedo, irradiance, environment, 1.0f));
+    }
+
+    public void Unload()
+    {
+        this.Content.Pop();
+    }
+}
