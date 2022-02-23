3df4ea38b74833e5d94a2981a696db13f66a7ed2
Roy Triesscheijn <roy-t@hotmail.com>
2022-02-23 22:43:21 +0100

    WIP Sunlight

---
+++ src/Mini.Engine.Content/Assets/Shaders/Lighting/Includes/Shadows.hlsl
@@ -0,0 +1,9 @@
+﻿#ifndef __SHADOWS
+#define __SHADOWS
+
+static const float BlendThreshold = 0.3f;
+static const float Bias = 0.005f; // Bias to prevent shadow acne
+static const uint NumCascades = 4;
+
+// TODO: copy from shadows.hlsl from the old project and check how to deal with the array and constants/parameters
+#endif
---
+++ src/Mini.Engine.Content/Assets/Shaders/Lighting/SunLight.hlsl
@@ -0,0 +1,51 @@
+﻿#include "../Includes/GBuffer.hlsl"
+#include "../Includes/Coordinates.hlsl"
+#include "Includes/Lights.hlsl"
+#include "Includes/Shadows.hlsl"
+
+// Use with FullScreenTriangle.TextureVS
+
+struct PS_INPUT
+{
+    float4 pos : SV_POSITION;
+    float2 tex : TEXCOORD;
+};
+
+cbuffer Constants : register(b0)
+{
+    float4 Color;
+    float3 SurfaceToLight;
+    float Strength;
+    float4x4 InverseViewProjection;
+    float3 CameraPosition;
+    float unused;
+}
+
+sampler TextureSampler : register(s0);
+Texture2D Albedo : register(t0);
+Texture2D Material : register(t1);
+Texture2D Depth : register(t2);
+Texture2D Normal : register(t3);
+
+#pragma PixelShader
+float4 PS(PS_INPUT input) : SV_TARGET
+{
+    float3 albedo = ReadAlbedo(Albedo, TextureSampler, input.tex);
+    float3 normal = ReadNormal(Normal, TextureSampler, input.tex);
+    float3 position = ReadPosition(Depth, TextureSampler, input.tex, InverseViewProjection);
+    Mat material = ReadMaterial(Material, TextureSampler, input.tex);
+
+    float3 worldPosition = ReadPosition(Depth, TextureSampler, input.tex, InverseViewProjection);
+    float depth = distance(worldPosition, CameraPosition);
+
+    float lightFactor = ComputeLightFactorPCF(worldPosition, depth);
+    float3 Lo = float3(0.0f, 0.0f, 0.0f);
+
+    if (lightFactor > 0)
+    {
+        // No attenuation since sunlight has already crossed an extreme distance
+        Lo = ComputeLight(albedo, normal, material, position, CameraPosition, SurfaceToLight, Color, Strength);
+    }    
+
+    return float4(Lo * lightFactor, 1.0f);
+}
