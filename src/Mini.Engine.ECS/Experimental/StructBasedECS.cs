48910474f87e03de31b97b416931db965ffff2fa
Roy Triesscheijn <roy-t@hotmail.com>
2022-07-25 21:03:58 +0200

    More work on struct based ecs

--- src/Mini.Engine.ECS/Experimental/StructBasedECS.cs
+++ src/Mini.Engine.ECS/Experimental/StructBasedECS.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mini.Engine.ECS.Experimental;
 
-public enum LifeCycle
+public enum LifeCycle : byte
 {
     Free,
     Created,
@@ -12,55 +12,138 @@ public enum LifeCycle
 
 public struct ComponentLifeCycle
 {
-    private LifeCycle current;
-    private LifeCycle next;
+    public LifeCycle Current;
+    public LifeCycle Next;
 
-    public bool IsAlive => this.current != LifeCycle.Free;
-    public bool IsFree => this.current == LifeCycle.Free;
+    public bool IsAlive => this.Current != LifeCycle.Free;
+    public bool IsFree => this.Current == LifeCycle.Free;
 
     public void New()
     {
-        this.current = LifeCycle.Created;
-        this.next = LifeCycle.New;
+        this.Current = LifeCycle.Created;
+        this.Next = LifeCycle.New;
     }
 
     public void Change()
     {
-        this.next = LifeCycle.Changed;
+        this.Next = LifeCycle.Changed;
     }
 
     public void Remove()
     {
-        this.next = LifeCycle.Removed;
+        this.Next = LifeCycle.Removed;
     }
 
-    public void Next()
+    public void Advance()
     {
-        this.current = this.next;
-        if (this.current == LifeCycle.Removed)
+        this.Current = this.Next;
+        if (this.Current == LifeCycle.Removed)
         {
-            this.next = LifeCycle.Free;
-        }
-        else if (this.current == LifeCycle.Free)
-        {
-            this.next = LifeCycle.Free;
+            this.Next = LifeCycle.Free;
         }
         else
         {
-            this.next = LifeCycle.Unchanged;
+            this.Next = LifeCycle.Unchanged;
         }
     }
 
     public override string ToString()
     {
-        return $"{this.current} -> {this.next}";
+        return $"{this.Current} -> {this.Next}";
     }
 }
 
 public interface IStructComponent
 {
-    ComponentLifeCycle LifeCycle { get; }
+    
 }
+
+public struct ComponentFilterEnumerable<T>
+    where T : struct, IStructComponent
+{
+    private readonly T[] Pool;
+    private readonly ComponentLifeCycle[] LifeCycles;
+    private readonly LifeCycle Filter;
+    private readonly int MaxIndex;
+    private int index;
+    
+    public ComponentFilterEnumerable(T[] pool, ComponentLifeCycle[] lifeCycles, LifeCycle filter, int maxIndex)
+    {
+        this.Pool = pool;
+        this.LifeCycles = lifeCycles;
+        this.Filter = filter;
+        this.MaxIndex = maxIndex;
+        this.index = -1;
+    }
+
+
+    public ComponentFilterEnumerable<T> GetEnumerator()
+    {
+        return this;
+    }
+
+    public ref T Current => ref this.Pool[this.index];
+
+    public bool MoveNext()
+    {
+        this.index++;
+
+        while (this.index <= this.MaxIndex)
+        {
+            if (this.LifeCycles[this.index].Current == this.Filter)
+            {
+                return true;
+            }
+
+            this.index++;
+        }
+
+        return false;
+    }
+}
+
+public struct AllComponentsEnumerable<T>
+    where T : struct, IStructComponent
+{
+    private readonly T[] Pool;
+    private readonly ComponentLifeCycle[] LifeCycles;
+    private readonly int MaxIndex;
+    private int index;
+
+    public AllComponentsEnumerable(T[] pool, ComponentLifeCycle[] lifeCycles, int maxIndex)
+    {
+        this.Pool = pool;
+        this.LifeCycles = lifeCycles;
+        this.index = -1;
+        this.MaxIndex = maxIndex;
+    }
+
+
+    public AllComponentsEnumerable<T> GetEnumerator()
+    {
+        return this;
+    }
+
+    public ref T Current => ref this.Pool[this.index];
+
+    public bool MoveNext()
+    {
+        this.index++;
+
+        while (this.index <= this.MaxIndex)
+        {
+            if (this.LifeCycles[this.index].Current != LifeCycle.Free)
+            {
+                return true;
+            }
+
+            this.index++;
+        }
+
+        return false;
+    }
+}
+
 
 public sealed class StructComponentContainer<T>
     where T : struct, IStructComponent
@@ -69,14 +152,26 @@ public sealed class StructComponentContainer<T>
 
     private int highestUsedSlot;
     private T[] pool;
+    private ComponentLifeCycle[] lifeCycles;
 
     public StructComponentContainer()
     {
         this.pool = new T[InitialBufferSize];
-        this.highestUsedSlot = 0;
+        this.lifeCycles = new ComponentLifeCycle[this.pool.Length];
+
+        this.highestUsedSlot = -1;
     }
 
     public int Count { get; private set; }
+
+    public float Fragmentation
+    {
+        get
+        {
+            if(this.Count == 0) { return 0.0f; }
+            return 1.0f - (this.highestUsedSlot / (float)this.Count);
+        }
+    }   
 
     public ref T Create()
     {
@@ -89,11 +184,28 @@ public sealed class StructComponentContainer<T>
 
         var index = this.IndexOfFirstUnused();
         this.highestUsedSlot = Math.Max(this.highestUsedSlot, index);
+                
+        this.lifeCycles[index].New();
+        return ref this.pool[index];
+    }
 
-        ref var component = ref this.pool[index];
-        component.LifeCycle.New();
+    public void AdvanceLifeCycles()
+    {
+        for (var i = 0; i < this.lifeCycles.Length; i++)
+        {
+            ref var lifeCycle = ref this.lifeCycles[i];
+            if (lifeCycle.Current == LifeCycle.Removed)
+            {
+                (this.pool[i] as IDisposable)?.Dispose();
 
-        return ref component;
+                if (i == this.highestUsedSlot)
+                {
+                    this.highestUsedSlot = this.IndexOfLastUsed(i - 1);
+                }
+            }
+
+            lifeCycle.Advance();
+        }
     }
 
     public void Defrag()
@@ -101,10 +213,15 @@ public sealed class StructComponentContainer<T>
         var lowestFreeSlot = 0;
         for (var i = this.highestUsedSlot; i > (this.Count - 1); i++)
         {
-            if (this.pool[i].LifeCycle.IsAlive)
+            if (this.lifeCycles[i].IsAlive)
             {
                 lowestFreeSlot = this.IndexOfFirstUnused(lowestFreeSlot);
+
                 this.pool[lowestFreeSlot] = this.pool[i];
+                this.lifeCycles[lowestFreeSlot] = this.lifeCycles[i];
+
+                this.lifeCycles[i].Current = LifeCycle.Free;
+                this.lifeCycles[i].Next = LifeCycle.Free;
             }
         }
 
@@ -116,11 +233,21 @@ public sealed class StructComponentContainer<T>
         }
     }
 
-    private int IndexOfFirstUnused(int offset = 0)
+    public AllComponentsEnumerable<T> EnumerateAll()
     {
-        for (var i = offset; i < this.pool.Length; i++)
+        return new AllComponentsEnumerable<T>(this.pool, this.lifeCycles, this.highestUsedSlot);
+    }
+
+    public ComponentFilterEnumerable<T> Enumerate(LifeCycle state)
+    {
+        return new ComponentFilterEnumerable<T>(this.pool, this.lifeCycles, state, this.highestUsedSlot);
+    }
+
+    private int IndexOfFirstUnused(int minIndex = 0)
+    {
+        for (var i = minIndex; i < this.pool.Length; i++)
         {
-            if (this.pool[i].LifeCycle.IsFree)
+            if (this.lifeCycles[i].IsFree)
             {
                 return i;
             }
@@ -129,14 +256,34 @@ public sealed class StructComponentContainer<T>
         return -1;
     }
 
+    private int IndexOfLastUsed(int maxIndex)
+    {
+        if (this.Count == 0)
+        {
+            return -1;
+        }
+
+        for (var i = maxIndex; i >= 0; i--)
+        {
+            if (this.lifeCycles[i].IsAlive)
+            {
+                return i;
+            }
+        }
+
+        throw new Exception($"Unreachable exception, could not find index of last used maxIndex: {maxIndex}, count {this.Count}");
+    }
+
     private void Grow()
     {
         Array.Resize(ref this.pool, this.pool.Length * 2);
+        Array.Resize(ref this.lifeCycles, this.pool.Length);
     }
 
     private void Shrink(int reserve = 0)
     {
         var minSize = Math.Max(this.Count, reserve);
         Array.Resize(ref this.pool, minSize);
-    }    
+        Array.Resize(ref this.lifeCycles, this.pool.Length);
+    }
 }
---
+++ src/Tests/Mini.Engine.Tests/StructBasedECSTests.cs
@@ -0,0 +1,57 @@
+﻿using Mini.Engine.ECS;
+using Mini.Engine.ECS.Experimental;
+using Xunit;
+using static Xunit.Assert;
+
+namespace Mini.Engine.Tests;
+
+public class StructBasedECSTests
+{
+    public struct Component : IStructComponent
+    {
+        public int MyValue { get; set; }
+    }
+
+    [Fact]
+    public void ContainerTest()
+    {
+        var container = new StructComponentContainer<Component>();
+
+        Equal(0, container.Count);
+        Equal(0.0f, container.Fragmentation);
+
+        ref var component = ref container.Create();
+        component.MyValue = 300;
+
+        var hit = false;
+        foreach(var c in container.EnumerateAll())
+        {
+            hit = true;
+            Equal(300, c.MyValue);
+        }
+
+        True(hit);
+
+        hit = false;
+        foreach (var c in container.Enumerate(LifeCycle.Created))
+        {
+            hit = true;
+            Equal(300, c.MyValue);
+        }
+
+        True(hit);
+
+        container.AdvanceLifeCycles();
+
+        hit = false;
+        foreach (var c in container.Enumerate(LifeCycle.New))
+        {
+            hit = true;
+            Equal(300, c.MyValue);
+        }
+
+        True(hit);
+
+        Equal(1, container.Count); //TODO: count is not properly updated
+    }
+}
