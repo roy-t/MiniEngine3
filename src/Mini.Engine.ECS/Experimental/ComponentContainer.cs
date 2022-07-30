42dc817d576d28cdbff32de306108ed554b9c883
Roy Triesscheijn <roy-t@hotmail.com>
2022-07-30 15:28:12 +0200

    WIP

--- src/Mini.Engine.ECS/Experimental/ComponentContainer.cs
+++ src/Mini.Engine.ECS/Experimental/ComponentContainer.cs
@@ -1,75 +1,75 @@
-﻿namespace Mini.Engine.ECS.Experimental;
+﻿//namespace Mini.Engine.ECS.Experimental;
 
-public interface IQuery<T>
-    where T : struct, IComponent
-{
-    public bool Accept(ref T component);
-}
+//public interface IQuery<T>
+//    where T : struct, IComponent
+//{
+//    public bool Accept(ref T component);
+//}
 
-public struct ResultIterator<T>
-    where T : struct, IComponent
-{
-    private readonly PoolAllocator<T> Pool;
-    private readonly IQuery<T> Query;
-    private int index;
+//public struct ResultIterator<T>
+//    where T : struct, IComponent
+//{
+//    private readonly PoolAllocator<T> Pool;
+//    private readonly IQuery<T> Query;
+//    private int index;
 
-    public ResultIterator(PoolAllocator<T> pool, IQuery<T> query)
-    {
-        this.Pool = pool;
-        this.Query = query;
-        this.index = -1;
-    }
+//    public ResultIterator(PoolAllocator<T> pool, IQuery<T> query)
+//    {
+//        this.Pool = pool;
+//        this.Query = query;
+//        this.index = -1;
+//    }
 
-    public ref T Current => ref this.Pool[this.index];
+//    public ref T Current => ref this.Pool[this.index];
 
-    public bool MoveNext()
-    {
-        while (this.index < this.Pool.HighestUsedSlot)
-        {
-            this.index++;
+//    public bool MoveNext()
+//    {
+//        while (this.index < this.Pool.HighestUsedSlot)
+//        {
+//            this.index++;
 
-            if (this.Pool.IsOccupied(this.index))
-            {
-                ref var component = ref this.Pool[this.index];
-                if (this.Query.Accept(ref component))
-                {
-                    return true;
-                }
-            }
-        }
+//            if (this.Pool.IsOccupied(this.index))
+//            {
+//                ref var component = ref this.Pool[this.index];
+//                if (this.Query.Accept(ref component))
+//                {
+//                    return true;
+//                }
+//            }
+//        }
 
-        return false;
-    }
-}
+//        return false;
+//    }
+//}
 
-public sealed class ComponentContainer<T>
-    where T : struct, IComponent
-{
-    private const int InitialCapacity = 10;
-    private readonly PoolAllocator<T> Pool;
+//public sealed class ComponentContainer<T>
+//    where T : struct, IComponent
+//{
+//    private const int InitialCapacity = 10;
+//    private readonly PoolAllocator<T> Pool;
 
-    // TODO: how to lookup components by entity
-    // we could let the pool allocator do insertion sort on entity id?
-    // and then a binary search on the entity?
-    // then give components a bitset that tracks which components it has?
+//    // TODO: how to lookup components by entity
+//    // we could let the pool allocator do insertion sort on entity id?
+//    // and then a binary search on the entity?
+//    // then give components a bitset that tracks which components it has?
 
-    public ComponentContainer()
-    {
-        this.Pool = new PoolAllocator<T>(InitialCapacity);
-    }
+//    public ComponentContainer()
+//    {
+//        this.Pool = new PoolAllocator<T>(InitialCapacity);
+//    }
 
-    public ref T Create(Entity entity)
-    {
-        return ref this.Pool.Create();
-    }
+//    public ref T Create(Entity entity)
+//    {
+//        return ref this.Pool.Create();
+//    }
 
-    public void Destroy(int index)
-    {
-        this.Pool.Destroy(index);
-    }
+//    public void Destroy(int index)
+//    {
+//        this.Pool.Destroy(index);
+//    }
 
-    public ResultIterator<T> Query(IQuery<T> query)
-    {
-        return new ResultIterator<T>(this.Pool, query);
-    }
-}
+//    public ResultIterator<T> Query(IQuery<T> query)
+//    {
+//        return new ResultIterator<T>(this.Pool, query);
+//    }
+//}
--- src/Tests/Mini.Engine.Tests/PoolAllocatorTests.cs
+++ src/Tests/Mini.Engine.Tests/PoolAllocatorTests.cs
@@ -1,80 +1,80 @@
-﻿using Mini.Engine.ECS;
-using Mini.Engine.ECS.Experimental;
-using Xunit;
-using static Xunit.Assert;
+﻿//using Mini.Engine.ECS;
+//using Mini.Engine.ECS.Experimental;
+//using Xunit;
+//using static Xunit.Assert;
 
-namespace Mini.Engine.Tests;
-public class PoolAllocatorTests
-{
-    public struct Component : IComponent
-    {
-        public Component(Entity entity, int value)
-        {
-            this.Entity = entity;
-            this.Value = value;
-        }
+//namespace Mini.Engine.Tests;
+//public class PoolAllocatorTests
+//{
+//    public struct Component : IComponent
+//    {
+//        public Component(Entity entity, int value)
+//        {
+//            this.Entity = entity;
+//            this.Value = value;
+//        }
 
-        public Entity Entity { get; }
-        public int Value { get; set; }
+//        public Entity Entity { get; }
+//        public int Value { get; set; }
 
-        public void Destroy()
-        {
-            this.Value = -1;
-        }
+//        public void Destroy()
+//        {
+//            this.Value = -1;
+//        }
 
-        public override string ToString()
-        {
-            return $"{this.Value}, {this.Entity}";
-        }
-    }
+//        public override string ToString()
+//        {
+//            return $"{this.Value}, {this.Entity}";
+//        }
+//    }
 
-    [Fact]
-    public void SmokeTest()
-    {
-        var entity = new Entity(1);
+//    [Fact]
+//    public void SmokeTest()
+//    {
+//        var entity = new Entity(1);
 
-        var allocator = new PoolAllocator<Component>(10);
+//        var allocator = new PoolAllocator<Component>(10);
 
-        ref var component = ref allocator.CreateFor(entity);
-        component.Value = 1;
+//        ref var component = ref allocator.CreateFor(entity);
+//        component.Value = 1;
 
-        True(allocator.IsOccupied(0));
-        Equal(1, allocator.Count);
-        Equal(1, allocator[0].Value);
+//        True(allocator.IsOccupied(0));
+//        Equal(1, allocator.Count);
+//        Equal(1, allocator[0].Value);
 
-        allocator.DestroyFor(entity);
+//        allocator.DestroyFor(entity);
 
-        Equal(0, allocator.Count);
-        False(allocator.IsOccupied(0));
+//        Equal(0, allocator.Count);
+//        False(allocator.IsOccupied(0));
 
-        for (var i = 0; i < 20; i++)
-        {
-            var e = new Entity(i + 1000);
-            ref var c = ref allocator.CreateFor(e);
-            c.Value = i;
-        }
+//        for (var i = 0; i < 20; i++)
+//        {
+//            var e = new Entity(i + 1000);
+//            ref var c = ref allocator.CreateFor(e);
+//            c.Value = i;
+//        }
 
-        Equal(20, allocator.Count);
-        True(allocator.Capacity >= 20);
+//        Equal(20, allocator.Count);
+//        True(allocator.Capacity >= 20);
 
-        for (var i = 0; i < 20; i += 2)
-        {
-            var e = allocator[i].Entity;
-            allocator.DestroyFor(e);
-        }
+//        for (var i = 0; i < 20; i += 2)
+//        {
+//            var e = allocator[i].Entity;
+//            allocator.DestroyFor(e);
+//        }
 
-        Equal(10, allocator.Count);        
-        Equal(0.5f, allocator.Fragmentation);
+//        Equal(10, allocator.Count);        
+//        Equal(0.5f, allocator.Fragmentation);
 
-        allocator.Vacuum();
+//        allocator.Vacuum();
 
-        Equal(10, allocator.Count);        
-        Equal(0.0f, allocator.Fragmentation);
+//        Equal(10, allocator.Count);        
+//        Equal(0.0f, allocator.Fragmentation);
 
-        allocator.Reserve(500);
-        Equal(500, allocator.Capacity);
+//        allocator.Reserve(500);
+//        Equal(500, allocator.Capacity);
 
-        allocator.Vacuum();
-        True(allocator.Capacity < 500);
-    }
-}
+//        allocator.Vacuum();
+//        True(allocator.Capacity < 500);
+//    }
+//}
