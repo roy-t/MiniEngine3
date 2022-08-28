d68a1fe809d0589c74dd3e1137b92d04222e41a4
Roy Triesscheijn <roy-t@hotmail.com>
2022-08-28 19:27:06 +0200

    wip simplify shader

--- src/Mini.Engine.Content/Assets/Shaders/World/Grass.hlsl
+++ src/Mini.Engine.Content/Assets/Shaders/World/Grass.hlsl
@@ -156,25 +156,13 @@ float3 Slerp(float3 p0, float3 p1, float t)
     return P;
 }
 
-#pragma VertexShader
-PS_INPUT VS(uint vertexId : SV_VertexID, uint instanceId : SV_InstanceID)
+
+float2 GetFacingDirection(InstanceData instance)
 {
-    PS_INPUT output;
-           
-    float3 tint = Instances[instanceId].tint;
+    float3 viewDirection = instance.position - CameraPosition;
     
-    float isEven = vertexId % 2 == 0;
-    float isOdd = 1.0f - isEven;
-        
-    float si = (vertexId / 2.0f) * isEven + ((vertexId - 1.0f) / 2.0f) * isOdd;
-    float s = si / 3.0f;
-      
-    InstanceData data = Instances[instanceId]; 
-        
-    float3 viewDirection = data.position - CameraPosition;
-    
-    float r = data.rotation;
-    float2 facingDirection = float2(sin(r), cos(r));    
+    float r = instance.rotation;
+    float2 facingDirection = float2(sin(r), cos(r));
     float2 viewTangent = normalize(float2(viewDirection.z, -viewDirection.x));
     float viewTangentDotFacing = dot(viewTangent, facingDirection);
                 
@@ -186,42 +174,161 @@ PS_INPUT VS(uint vertexId : SV_VertexID, uint instanceId : SV_InstanceID)
     // so it either has to drastically flip left or right
     if (viewTangentDotFacing > 0)
     {
-        float2 foo = Slerp(facingDirection, viewTangent, l);
+        float2 foo = normalize(lerp(facingDirection, viewTangent, l));
         r = -atan2(foo.y, foo.x);
     }
     else
     {
-        float2 foo = Slerp(facingDirection, -viewTangent, l);
+        float2 foo = normalize(lerp(facingDirection, -viewTangent, l));
         r = -atan2(foo.y, foo.x);
     }
     
+    return float2(sin(r), cos(r));
+}
+
+
+void GetSpinePosition(uint vertexId, float tilt, out float2 position, out float2 normal)
+{
+    float isEven = vertexId % 2 == 0;
+    float isOdd = vertexId % 2 != 0;
     
-    float4x4 world = CreateMatrix(r, data.position, data.scale);
-    float3x3 rotation = (float3x3) world;
-    
-    float3 worldPosition = mul(world, float4(0, 0, 0, 1)).xyz;
-    //float3 facingDirection = mul(rotation, float3(0, 0, -1));
-    float tilt = GetTiltFromWind(worldPosition, facingDirection);
+    float si = (vertexId / 2.0f) * isEven + ((vertexId - 1.0f) / 2.0f) * isOdd;
     float2 p;
     float2 n;
-    Segments(si, tilt, 0.33f, 3.0f, 4, p, n);
+    //Segments(si, tilt, 0.33f, 3.0f, 4, p, n);
     
-    const float width = 0.06f;
-    float4 position = float4(p.x + isOdd * width, p.y, 0.0f, 1.0f);
-    float2 texcoord = float2(isOdd, 1.0f - s);
+    //float segment, float targetAngle, float length, float stiffness, float totalSegments
+    float segment = si;
+    float targetAngle = tilt;
+    float length = 0.33f;
+    float stiffness = 3.0f;
+    float totalSegments = 4;
     
-    // Pull the normals up a bit to shade them as a semi-uniform plane
+    float2 accum = float2(0, 0);
+    float angle = 0.0f;
+    for (float i = 0; i <= segment; i += 1.0f)
+    {
+        float flexibility = 0.001f + (i / (totalSegments - 1.0f));
+        float e = pow(2, stiffness * flexibility - stiffness);
+        angle = (targetAngle / totalSegments) * (i + 1) * e;
+        float2 dir = float2(sin(angle), cos(angle));
+        accum += dir * length;
+
+    }
+    
+    position = accum;
+    normal = float2(sin(angle - PI_OVER_TWO), cos(angle - PI_OVER_TWO));
+}
+
+float3 BiasNormal(float2 n, float3x3 rotation, float bias)
+{
     float3 normal = float3(n.x, n.y, 0.0f);
-    normal = normalize(mul(rotation, normal));            
-    normal = normalize(lerp(normal, float3(0, 1, 0), 0.33f));    
+    normal = normalize(mul(rotation, normal));
+    normal = normalize(lerp(normal, float3(0, 1, 0), bias));
+    
+    return normal;
+}
+
+#pragma VertexShader
+PS_INPUT VS(uint vertexId : SV_VertexID, uint instanceId : SV_InstanceID)
+{
+    PS_INPUT output;
+    
+    InstanceData data = Instances[instanceId];
+    
+    float2 facing = GetFacingDirection(data);
+    float tilt = GetTiltFromWind(data.position, facing);
+    
+    float2 spinePosition;
+    float2 spineNormal;
+    
+    GetSpinePosition(vertexId, tilt, spinePosition, spineNormal);
+        
+    const float halfWidth = 0.03f;
+    float side = ((vertexId % 2 == 0) * 2) - 1.0f;
+    
+    float4 position = float4(spinePosition.x + (side * halfWidth), spinePosition.y, 0.0f, 1.0f);
+    float2 texcoord = float2(side + 1.0f, 0.0f);
+    
+    float4x4 world = CreateMatrix(atan2(facing.y, facing.x), data.position, data.scale);
+    float3x3 rotation = (float3x3) world;
+    
+    
+    float3 normal = BiasNormal(spineNormal, rotation, 0.33f);
     
     output.position = mul(mul(ViewProjection, world), position);
     output.world = mul(world, position).xyz;
     output.texcoord = texcoord;
     output.normal = normal;
-    output.tint = tint;
+    output.tint = data.tint;
 
     return output;
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // TODO: double check if everything still works, especiall the facing bias that makes sure you never see thin sides
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+           
+//    float3 tint = Instances[instanceId].tint;
+    
+//    float isEven = vertexId % 2 == 0;
+//    float isOdd = 1.0f - isEven;
+        
+//    float si = (vertexId / 2.0f) * isEven + ((vertexId - 1.0f) / 2.0f) * isOdd;
+//    float s = si / 3.0f;
+      
+//    InstanceData data = Instances[instanceId]; 
+        
+//    float3 viewDirection = data.position - CameraPosition;
+    
+//    float r = data.rotation;
+//    float2 facingDirection = float2(sin(r), cos(r));    
+//    float2 viewTangent = normalize(float2(viewDirection.z, -viewDirection.x));
+//    float viewTangentDotFacing = dot(viewTangent, facingDirection);
+                
+//    float l = (1.0f - abs(viewTangentDotFacing)) * 0.25f;
+//    //l = 0.0f;
+    
+//    // TODO: this makes grass flip direction
+//    // when its very close to the view direction
+//    // so it either has to drastically flip left or right
+//    if (viewTangentDotFacing > 0)
+//    {
+//        float2 foo = normalize(lerp(facingDirection, viewTangent, l));
+//        r = -atan2(foo.y, foo.x);
+//    }
+//    else
+//    {
+//        float2 foo = normalize(lerp(facingDirection, -viewTangent, l));
+//        r = -atan2(foo.y, foo.x);
+//    }
+    
+    
+//    float4x4 world = CreateMatrix(r, data.position, data.scale);
+//    float3x3 rotation = (float3x3) world;
+    
+//    float3 worldPosition = mul(world, float4(0, 0, 0, 1)).xyz;
+//    //float3 facingDirection = mul(rotation, float3(0, 0, -1));
+//    float tilt = GetTiltFromWind(worldPosition, facingDirection);
+//    float2 p;
+//    float2 n;
+//    Segments(si, tilt, 0.33f, 3.0f, 4, p, n);
+    
+//    const float width = 0.06f;
+//    float4 position = float4(p.x + isOdd * width, p.y, 0.0f, 1.0f);
+//    float2 texcoord = float2(isOdd, 1.0f - s);
+    
+//    // Pull the normals up a bit to shade them as a semi-uniform plane
+//    float3 normal = float3(n.x, n.y, 0.0f);
+//    normal = normalize(mul(rotation, normal));            
+//    normal = normalize(lerp(normal, float3(0, 1, 0), 0.33f));    
+    
+//    output.position = mul(mul(ViewProjection, world), position);
+//    output.world = mul(world, position).xyz;
+//    output.texcoord = texcoord;
+//    output.normal = normal;
+//    output.tint = tint;
+
+//    return output;
 }
 
 #pragma PixelShader
