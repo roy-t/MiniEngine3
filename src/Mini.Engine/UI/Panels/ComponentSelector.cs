ceaa342380d3562c45780a3ab3f96f5fc8a2ad91
Roy Triesscheijn <roy-t@hotmail.com>
2022-09-17 14:28:18 +0200

    Start UI for grass

---
+++ src/Mini.Engine/UI/Panels/ComponentSelector.cs
@@ -0,0 +1,67 @@
+﻿using ImGuiNET;
+using Mini.Engine.ECS;
+using Mini.Engine.ECS.Components;
+
+namespace Mini.Engine.UI.Panels;
+internal sealed class ComponentSelector<T>
+    where T : struct, IComponent
+{
+
+    private readonly IComponentContainer<T> Container;
+    private readonly List<Entity> Entities;
+
+    private int selected;
+
+    public ComponentSelector(string title, IComponentContainer<T> container)
+    {
+        this.Title = title;
+        this.Container = container;
+        this.Entities = new List<Entity>();
+    }
+
+    public string Title { get; set; }
+
+    public void Update()
+    {
+        this.Entities.Clear();
+
+        var iterator = this.Container.IterateAll();
+        while (iterator.MoveNext())
+        {
+            ref var component = ref iterator.Current;
+            this.Entities.Add(component.Entity);
+        }
+
+        Entity? selectedEntity = null;
+        this.selected = Math.Max(0, Math.Min(this.selected, this.Entities.Count - 1));
+
+        if (this.Entities.Any())
+        {
+            selectedEntity = this.Entities[this.selected];
+        }
+
+        if (ImGui.BeginCombo(this.Title, selectedEntity?.ToString() ?? string.Empty))
+        {
+            for (var i = 0; i < this.Entities.Count; i++)
+            {
+                var entity = this.Entities[i];
+                if (ImGui.Selectable(entity.ToString(), entity == selectedEntity))
+                {
+                    this.selected = i;
+                }
+            }
+
+            ImGui.EndCombo();
+        }
+    }
+
+    public bool HasComponent()
+    {
+        return this.selected < this.Entities.Count;
+    }
+
+    public ref T Get()
+    {
+        return ref this.Container[this.Entities[this.selected]];
+    }
+}
---
+++ src/Mini.Engine/UI/Panels/GrassPanel.cs
@@ -0,0 +1,65 @@
+﻿using ImGuiNET;
+using Mini.Engine.Configuration;
+using Mini.Engine.Content;
+using Mini.Engine.DirectX;
+using Mini.Engine.ECS;
+using Mini.Engine.ECS.Components;
+using Mini.Engine.Graphics.Vegetation;
+using Mini.Engine.Graphics.World;
+
+namespace Mini.Engine.UI.Panels;
+
+[Service]
+internal sealed class GrassPanel : IPanel
+{
+    private readonly Device Device;
+    private readonly ContentManager Content;
+    private readonly ECSAdministrator Administrator;
+
+    private readonly ComponentSelector<GrassComponent> GrassComponentSelector;
+    private readonly ComponentSelector<TerrainComponent> TerrainComponentSelector;
+
+
+    public GrassPanel(Device device, ContentManager content, ECSAdministrator administrator, ContainerStore containerStore)
+    {
+        this.Device = device;
+        this.Content = content;
+        this.Administrator = administrator;
+
+        this.GrassComponentSelector = new ComponentSelector<GrassComponent>("Grass Component", containerStore.GetContainer<GrassComponent>());
+        this.TerrainComponentSelector = new ComponentSelector<TerrainComponent>("Terrain Component", containerStore.GetContainer<TerrainComponent>());
+    }
+
+    public string Title => "Grass";
+
+    public void Update(float elapsed)
+    {
+        this.GrassComponentSelector.Update();
+
+        ImGui.Separator();
+
+        if (this.GrassComponentSelector.HasComponent())
+        {
+            ref var component = ref this.GrassComponentSelector.Get();
+
+            if(ImGui.BeginTable("GrassComponentTable", 2))
+            {
+                ImGui.TableSetupColumn("Property");
+                ImGui.TableSetupColumn("Value");
+                ImGui.TableHeadersRow();
+
+                ImGui.TableNextColumn();
+                ImGui.Text("Instances");
+                ImGui.TableNextColumn();
+                ImGui.Text(component.Instances.ToString());
+
+                ImGui.EndTable();
+            }
+        }
+
+        this.TerrainComponentSelector.Update();
+    }
+
+    // TODO: create a compute shader that generates the position information for each grass blade
+    // based on the selected terrain's 
+}
