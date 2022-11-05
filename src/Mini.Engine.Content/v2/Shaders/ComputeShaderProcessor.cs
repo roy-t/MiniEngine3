6c1ed8d4b3c66c4a25f96ccdd5708cf2bdb5023d
Roy Triesscheijn <roy-t@hotmail.com>
2022-11-05 10:06:05 +0100

    make it easier to write shader processors

--- src/Mini.Engine.Content/v2/Shaders/ComputeShaderProcessor.cs
+++ src/Mini.Engine.Content/v2/Shaders/ComputeShaderProcessor.cs
@@ -1,64 +1,41 @@
 ﻿using Mini.Engine.Content.v2.Serialization;
 using Mini.Engine.DirectX;
 using Mini.Engine.DirectX.Resources.Shaders;
-using Vortice.D3DCompiler;
-using Vortice.Direct3D;
 
 namespace Mini.Engine.Content.v2.Shaders;
-internal sealed class ComputeShaderProcessor : IContentProcessor<IComputeShader, ComputeShaderContent, ComputeShaderSettings>
+internal sealed class ComputeShaderProcessor : ShaderProcessor<IComputeShader, ComputeShaderContent, ComputeShaderSettings>
 {
     private static readonly Guid HeaderComputeShader = new("{8891B57B-C52C-4933-B121-FE6C718DB3D7}");
-    private static readonly ShaderMacro[] Defines = Array.Empty<ShaderMacro>();
-    private static readonly string Profile = "cs_5_0";
-
-    private readonly Device Device;
+    private const int VersionComputeShader = 1;
 
     public ComputeShaderProcessor(Device device)
+        : base(device, HeaderComputeShader, VersionComputeShader) { }
+
+    public override string Profile => "cs_5_0";
+
+    protected override void WriteSettings(ContentWriter writer, ComputeShaderSettings settings)
     {
-        this.Device = device;
-        this.Cache = new ContentTypeCache<IComputeShader>();
-    }
-
-    public IContentTypeCache<IComputeShader> Cache { get; }
-    public int Version => 1;
-
-    public void Generate(ContentId id, ComputeShaderSettings settings, ContentWriter writer, TrackingVirtualFileSystem fileSystem)
-    {
-        var sourceText = fileSystem.ReadAllText(id.Path);
-        using var include = new ShaderFileInclude(fileSystem, Path.GetDirectoryName(id.Path));
-
-        Compiler.Compile(sourceText, Defines, include, id.Key, id.Path, Profile, out var shaderBlob, out var errorBlob);
-        ShaderCompilationErrorFilter.ThrowOnWarningOrError(errorBlob, "X3568" /*Undefined Pragma */);
-
-        writer.WriteHeader(HeaderComputeShader, this.Version, fileSystem.GetDependencies());
         writer.Writer.Write(settings.NumThreadsX);
         writer.Writer.Write(settings.NumThreadsY);
         writer.Writer.Write(settings.NumThreadsZ);
-        writer.WriteArray(shaderBlob.AsSpan());
-
-        shaderBlob?.Dispose();
-        errorBlob?.Dispose();
     }
 
-    public IComputeShader Load(ContentId contentId, ContentHeader header, ContentReader reader)
+    protected override IComputeShader Load(ContentId contentId, ComputeShaderSettings settings, byte[] byteCode)
     {
-        ContentProcessor.ValidateHeader(HeaderComputeShader, this.Version, header);
+        return new ComputeShader(this.Device, byteCode, settings.NumThreadsX, settings.NumThreadsY, settings.NumThreadsZ);
+    }
 
+    protected override ComputeShaderSettings LoadSetings(ContentReader reader)
+    {
         var numThreadsX = reader.Reader.ReadInt32();
         var numThreadsY = reader.Reader.ReadInt32();
         var numThreadsZ = reader.Reader.ReadInt32();
-        var byteCode = reader.ReadArray();
 
-        return new ComputeShader(this.Device, byteCode, numThreadsX, numThreadsY, numThreadsZ);
+        return new ComputeShaderSettings(numThreadsX, numThreadsY, numThreadsZ);
     }
 
-    public ComputeShaderContent Wrap(ContentId id, IComputeShader content, ComputeShaderSettings settings, ISet<string> dependencies)
+    public override ComputeShaderContent Wrap(ContentId id, IComputeShader content, ComputeShaderSettings settings, ISet<string> dependencies)
     {
         return new ComputeShaderContent(id, content, settings, dependencies);
-    }
-
-    public void Reload(IContent original, ContentWriterReader writerReader, TrackingVirtualFileSystem fileSystem)
-    {
-        ContentReloader.Reload(this, (ComputeShaderContent)original, fileSystem, writerReader);
     }
 }
---
+++ src/Mini.Engine.Content/v2/Shaders/ShaderProcessor.cs
@@ -0,0 +1,68 @@
+﻿using Mini.Engine.Content.v2.Serialization;
+using Mini.Engine.DirectX;
+using Mini.Engine.DirectX.Resources.Shaders;
+using Vortice.D3DCompiler;
+using Vortice.Direct3D;
+
+namespace Mini.Engine.Content.v2.Shaders;
+internal abstract class ShaderProcessor<TContent, TWrapped, TSettings> : IContentProcessor<TContent, TWrapped, TSettings>
+    where TContent : IShader, IDisposable
+    where TWrapped : IContent<TContent, TSettings>, TContent
+{
+    private static readonly ShaderMacro[] Defines = Array.Empty<ShaderMacro>();
+
+    protected readonly Device Device;
+
+    public ShaderProcessor(Device device, Guid typeHeader, int version)
+    {
+        this.Device = device;
+        this.Cache = new ContentTypeCache<TContent>();
+
+        this.TypeHeader = typeHeader;
+        this.Version = version;
+    }
+
+    public IContentTypeCache<TContent> Cache { get; }
+    public Guid TypeHeader {get;}
+    public int Version { get; }
+
+    public abstract string Profile { get; }
+
+    public void Generate(ContentId id, TSettings settings, ContentWriter writer, TrackingVirtualFileSystem fileSystem)
+    {
+        var sourceText = fileSystem.ReadAllText(id.Path);
+        using var include = new ShaderFileInclude(fileSystem, Path.GetDirectoryName(id.Path));
+
+        Compiler.Compile(sourceText, Defines, include, id.Key, id.Path, this.Profile, out var shaderBlob, out var errorBlob);
+        ShaderCompilationErrorFilter.ThrowOnWarningOrError(errorBlob, "X3568" /*Undefined Pragma */);
+
+        writer.WriteHeader(this.TypeHeader, this.Version, fileSystem.GetDependencies());
+        this.WriteSettings(writer, settings);        
+        writer.WriteArray(shaderBlob.AsSpan());
+
+        shaderBlob?.Dispose();
+        errorBlob?.Dispose();
+    }
+
+    protected abstract void WriteSettings(ContentWriter writer, TSettings settings);
+
+    public TContent Load(ContentId contentId, ContentHeader header, ContentReader reader)
+    {
+        ContentProcessor.ValidateHeader(this.TypeHeader, this.Version, header);
+        var settings = this.LoadSetings(reader);        
+        var byteCode = reader.ReadArray();
+
+        return this.Load(contentId, settings, byteCode);        
+    }
+
+    protected abstract TSettings LoadSetings(ContentReader reader);
+
+    protected abstract TContent Load(ContentId contentId, TSettings settings, byte[] byteCode);
+
+    public abstract TWrapped Wrap(ContentId id, TContent content, TSettings settings, ISet<string> dependencies);
+
+    public void Reload(IContent original, ContentWriterReader writerReader, TrackingVirtualFileSystem fileSystem)
+    {
+        ContentReloader.Reload(this, (TWrapped)original, fileSystem, writerReader);
+    }    
+}
