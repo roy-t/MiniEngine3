3ce8be56361e6c583c1b600d30b38b54a1fae096
roy-t <roy-t@hotmail.com>
2023-05-11 16:50:56 +0200

    fix performance issues with performance counters (how ironic)

--- src/Backend/Mini.Engine.Debugging/PerformanceCounters.cs
+++ src/Backend/Mini.Engine.Debugging/PerformanceCounters.cs
@@ -10,16 +10,50 @@ internal sealed class PerformanceCounters : IDisposable
     public readonly PerformanceAggregator CPUMemoryCounter;
     public readonly PerformanceAggregator CPUUsageCounter;
 
+    private readonly Thread MonitorThread;
+    private bool isRunning;
+
     public PerformanceCounters()
     {
         this.GPUMemoryCounter = new PerformanceAggregator("GPU Process Memory", "Dedicated Usage");
         this.GPUUsageCounter = new PerformanceAggregator("GPU Engine", "Utilization Percentage", instanceFilter: "engtype_3D");
         this.CPUMemoryCounter = new PerformanceAggregator("Process", "Working Set - Private");
         this.CPUUsageCounter = new PerformanceAggregator("Process", "% Processor Time", scale: 1.0f / Environment.ProcessorCount);
+
+        this.MonitorThread = new Thread(() =>
+        {
+            var counters = new PerformanceAggregator[] { this.GPUMemoryCounter, this.GPUUsageCounter, this.CPUMemoryCounter, this.CPUUsageCounter };
+
+            foreach (var counter in counters)
+            {
+                counter.InitializeCounter();
+            }
+
+            while (this.isRunning)
+            {
+                foreach (var counter in counters)
+                {
+                    if (this.isRunning)
+                    {
+                        counter.Update();
+                    }
+                }
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(20));
+            }
+        });
+
+        this.isRunning = true;
+        this.MonitorThread.IsBackground = true;
+        this.MonitorThread.Priority = ThreadPriority.Lowest;
+        this.MonitorThread.Start();
     }
 
     public void Dispose()
     {
+        this.isRunning = false;
+        this.MonitorThread.Join();
+
         this.GPUMemoryCounter.Dispose();
         this.GPUUsageCounter.Dispose();
 
@@ -29,48 +63,41 @@ internal sealed class PerformanceCounters : IDisposable
 
     public sealed class PerformanceAggregator : IDisposable
     {
-        private bool initializedCounter;
         private PerformanceCounter? counter;
-        private Timer? timer;
-
-        private readonly float Scale;
 
         public PerformanceAggregator(string categoryName, string counterName, string? instanceFilter = null, float scale = 1.0f)
         {
+            this.CategoryName = categoryName;
+            this.CounterName = counterName;
+            this.InstanceFilter = instanceFilter ?? string.Empty;
             this.Scale = scale;
-            this.timer = new Timer(_ =>
-            {
-                if (!this.initializedCounter)
-                {                    
-                    this.InitializeCounter(categoryName, counterName, instanceFilter);
-                    this.initializedCounter = true;
-                }
+        }
+        public float Value { get; private set; }
 
-                if (this.counter == null)
-                {
-                    this.Dispose();
-                }
-                else
-                {
-                    this.Value = (this.counter?.NextValue() ?? 0.0f) * this.Scale;
-                }
-            }, null, TimeSpan.FromMilliseconds(20), TimeSpan.FromMilliseconds(20));
+        public string CategoryName { get; }
+        public string CounterName { get; }
+        public string InstanceFilter { get; }
+        public float Scale { get; }
+
+        internal void Update()
+        {
+            this.Value = (this.counter?.NextValue() ?? 0.0f) * this.Scale;
         }
 
-        private void InitializeCounter(string categoryName, string counterName, string? instanceFilter)
+        internal void InitializeCounter()
         {
-            if (PerformanceCounterCategory.CounterExists(counterName, categoryName))
+            if (PerformanceCounterCategory.CounterExists(this.CounterName, this.CategoryName))
             {
-                var category = new PerformanceCounterCategory(categoryName);
+                var category = new PerformanceCounterCategory(this.CategoryName);
                 if (category.CategoryType == PerformanceCounterCategoryType.MultiInstance)
                 {
                     var processName = Process.GetCurrentProcess().ProcessName;
                     var processId = Environment.ProcessId.ToString();
 
                     IEnumerable<string> names = category.GetInstanceNames();
-                    if (!string.IsNullOrEmpty(instanceFilter))
+                    if (!string.IsNullOrEmpty(this.InstanceFilter))
                     {
-                        names = names.Where(n => n.Contains(instanceFilter));
+                        names = names.Where(n => n.Contains(this.InstanceFilter));
                     }
 
                     var instanceName = names
@@ -79,25 +106,20 @@ internal sealed class PerformanceCounters : IDisposable
 
                     if (instanceName != null)
                     {
-                        this.counter = new PerformanceCounter(categoryName, counterName, instanceName, true);
+                        this.counter = new PerformanceCounter(this.CategoryName, this.CounterName, instanceName, true);
                     }
                 }
                 else
                 {
-                    this.counter = new PerformanceCounter(categoryName, counterName, true);
+                    this.counter = new PerformanceCounter(this.CategoryName, this.CounterName, true);
                 }
             }
         }
 
-        public float Value { get; private set; }
-
         public void Dispose()
         {
-            this.timer?.Dispose();
-            this.timer = null;
-
-            this.counter = null;
             this.counter?.Dispose();
+            this.counter = null;
         }
     }
 }
