b17a51f4ee8d687a8414d7b4db4ffac17eaacb3d
Roy Triesscheijn <roy-t@hotmail.com>
2024-02-03 17:12:24 +0100

    Color all rows

--- src/Mini.Engine/Titan/Graphics/ZoneTerrainColorizer.cs
+++ src/Mini.Engine/Titan/Graphics/ZoneTerrainColorizer.cs
@@ -14,6 +14,7 @@ public sealed class ZoneTerrainColorizer : ITerrainColorizer
 
     private readonly int[] Owners;
     private Modes mode;
+    private bool expand;
     private int back;
     private int front;
     private int zone;
@@ -24,6 +25,7 @@ public sealed class ZoneTerrainColorizer : ITerrainColorizer
 
         this.Owners = new int[columns * rows];
         this.mode = Modes.Explore;
+        this.expand = false;
         this.back = 0;
         this.front = 0;
         this.zone = 0;
@@ -36,7 +38,7 @@ public sealed class ZoneTerrainColorizer : ITerrainColorizer
         Debug.Assert(columns > 0);
         Debug.Assert(rows > 0);
 
-        // Treat first column seperately
+        // Treat first row seperately as we don't have to check if we can expand the zone in the previous row
         while (this.back < columns)
         {
             switch (this.mode)
@@ -78,6 +80,57 @@ public sealed class ZoneTerrainColorizer : ITerrainColorizer
                         this.mode = Modes.Explore;
                     }
                     break;
+            }
+        }
+
+        for (var r = 1; r < rows; r++)
+        {
+            var offset = columns * r;
+            this.back = 0;
+            this.front = 0;
+
+            while (this.back < columns)
+            {
+                switch (this.mode)
+                {
+                    case Modes.Explore:
+                        var back = tiles[this.back + offset];
+                        var front = tiles[this.front + offset];
+
+                        var eol = this.front >= (columns - 1);
+                        var connected = front.Offset == back.Offset &&
+                            front.IsLevel() && back.IsLevel();
+
+                        if (eol)
+                        {
+                            this.front += 1;
+                            this.mode = Modes.Catchup;
+                        }
+                        else if (connected)
+                        {
+                            this.front += 1;
+                        }
+                        else
+                        {
+                            this.mode = Modes.Catchup;
+                        }
+
+                        break;
+
+                    case Modes.Catchup:
+                        if (this.back < this.front)
+                        {
+                            this.Owners[this.back + offset] = this.zone;
+                            this.back += 1;
+                        }
+                        else
+                        {
+                            this.zone += 1;
+                            this.front += 1;
+                            this.mode = Modes.Explore;
+                        }
+                        break;
+                }
             }
         }
     }
