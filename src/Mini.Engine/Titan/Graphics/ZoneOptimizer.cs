d21f2601c982396d78faefe4fbd9ac597872507e
Roy Triesscheijn <roy-t@hotmail.com>
2024-02-04 16:35:47 +0100

    fix optimizer

---
+++ src/Mini.Engine/Titan/Graphics/ZoneOptimizer.cs
@@ -0,0 +1,159 @@
+﻿using LibGame.Mathematics;
+
+namespace Mini.Engine.Titan.Graphics;
+
+public sealed class ZoneOptimizer
+{
+    public enum Modes
+    {
+        Explore,
+        Catchup,
+    }
+
+    public readonly record struct Zone(int StartColumn, int EndColumn, int StartRow, int EndRow);
+
+    public class State(int columns, int rows)
+    {
+        public int[] Owners { get; } = new int[columns * rows];
+        public List<Zone> Zones { get; } = new List<Zone>();
+
+        internal Modes Mode = Modes.Explore;
+        internal int Back = 0;
+        internal int Front = 0;
+        internal int Zone = 0;
+        internal bool MatchVertical = false;
+    }
+
+    public static State Optimize(IReadOnlyList<Tile> tiles, int columns, int rows)
+    {
+        var state = new State(columns, rows);
+        for (var r = 0; r < rows; r++)
+        {
+            var offset = columns * r;
+            state.Back = 0;
+            state.Front = 0;
+
+            while (state.Back < columns)
+            {
+
+                if (r > 0)
+                {
+
+                }
+                switch (state.Mode)
+                {
+                    case Modes.Explore:
+
+                        var canAdvance = CanAdvanceFront(in state, tiles, offset, columns);
+                        if (canAdvance)
+                        {
+                            state.Front += 1;
+                        }
+                        else
+                        {
+                            state.MatchVertical = CanExpandNorthernZone(in state, tiles, offset, columns);
+                            state.Mode = Modes.Catchup;
+                        }
+
+                        break;
+
+                    case Modes.Catchup:
+                        var zone = state.Zone;
+                        if (state.MatchVertical)
+                        {
+                            zone = state.Owners[(state.Back + offset) - columns];
+                        }
+                        if (state.Back < state.Front || state.Front == (columns - 1))
+                        {
+                            state.Owners[state.Back + offset] = zone;
+                            state.Back += 1;
+                        }
+                        else
+                        {
+                            state.Zone += state.MatchVertical ? 0 : 1;
+                            state.Front += 1;
+                            state.Mode = Modes.Explore;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return state;
+    }
+
+    private static bool CanAdvanceFront(in State state, IReadOnlyList<Tile> tiles, int offset, int columns)
+    {
+        var hasNext = state.Front < (columns - 1);
+        var back = tiles[state.Back + offset];
+        var front = tiles[state.Front + offset];
+
+        return front.Offset == back.Offset &&
+            front.IsLevel() && back.IsLevel() && hasNext;
+    }
+
+
+    private static bool CanExpandNorthernZone(in State state, IReadOnlyList<Tile> tiles, int offset, int columns)
+    {
+        // There should be a row to the north
+        var (_, y) = Indexes.ToTwoDimensional(state.Front + offset, columns);
+        if (y == 0)
+        {
+            return false;
+        }
+
+        // Remember: front has already advanced to a position that doesn't match
+        var f = (state.Front - 1);
+        var b = state.Back;
+
+        // Special case for 1x1 strips, they could be on a slope in which case we don't want to add them to a zone
+        if (b == f)
+        {
+            var t = tiles[f + offset];
+            if (!t.IsLevel())
+            {
+                return false;
+            }
+            var n = tiles[f + offset - columns];
+            if (!n.IsLevel())
+            {
+                return false;
+            }
+            if (t.Offset != n.Offset)
+            {
+                return false;
+            }
+        }
+
+        // The zone north of our front and back should be the same
+        var zoneBackN = state.Owners[(b + offset) - columns];
+        var zoneFrontN = state.Owners[(f + offset) - columns];
+
+        if (zoneBackN != zoneFrontN)
+        {
+            return false;
+        }
+
+        // The zone NW of back should be different, if it exists
+        if (b > 0)
+        {
+            var zoneBackNW = state.Owners[((b + offset) - columns) - 1];
+            if (zoneBackNW == zoneBackN)
+            {
+                return false;
+            }
+        }
+
+        // The zone NE of front should be different, if it exists
+        if (f < (columns - 1))
+        {
+            var zoneFrontNE = state.Owners[((f + offset) - columns) + 1];
+            if (zoneFrontNE == zoneFrontN)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- src/Mini.Engine/Titan/Graphics/ZoneTerrainColorizer.cs
+++ src/Mini.Engine/Titan/Graphics/ZoneTerrainColorizer.cs
@@ -1,152 +1,27 @@
-﻿using System.Diagnostics;
-using LibGame.Graphics;
+﻿using LibGame.Graphics;
+using LibGame.Mathematics;
 
 namespace Mini.Engine.Titan.Graphics;
 public sealed class ZoneTerrainColorizer : ITerrainColorizer
 {
-    public enum Modes
-    {
-        Explore,
-        Catchup,
-    }
-
-    private readonly ColorPalette ColorPalette;
-
-    private readonly int[] Owners;
-    private Modes mode;
-    private int back;
-    private int front;
-    private int zone;
-    private bool matchAbove;
-
+    private readonly ZoneOptimizer.State State;
+    private readonly ColorLinear[] AllColors;
     public ZoneTerrainColorizer(Tile[] tiles, int columns, int rows)
     {
-        this.ColorPalette = ColorPalette.GrassLawn;
+        this.State = ZoneOptimizer.Optimize(tiles, columns, rows);
 
-        this.Owners = new int[columns * rows];
-        this.mode = Modes.Explore;
-        this.back = 0;
-        this.front = 0;
-        this.zone = 0;
+        this.AllColors = new ColorLinear[256 * 256 * 256];
 
-        this.Ininitialize(tiles, columns, rows);
-    }
+        // Horribly in efficient way to do this if we already know how many tiles we have :P
 
-    private void Ininitialize(Tile[] tiles, int columns, int rows)
-    {
-        Debug.Assert(columns > 0);
-        Debug.Assert(rows > 0);
-
-        // Treat first row seperately as we don't have to check if we can expand the zone in the previous row
-        while (this.back < columns)
+        var i = 0;
+        for (var r = 0; r < 256; r++)
         {
-            switch (this.mode)
+            for (var g = 0; g < 256; g++)
             {
-                case Modes.Explore:
-                    var back = tiles[this.back];
-                    var front = tiles[this.front];
-
-                    var eol = this.front >= (columns - 1);
-                    var connected = front.Offset == back.Offset &&
-                        front.IsLevel() && back.IsLevel();
-
-                    if (eol)
-                    {
-                        this.front += 1;
-                        this.mode = Modes.Catchup;
-                    }
-                    else if (connected)
-                    {
-                        this.front += 1;
-                    }
-                    else
-                    {
-
-                        this.mode = Modes.Catchup;
-                    }
-
-                    break;
-
-                case Modes.Catchup:
-                    if (this.back < this.front)
-                    {
-                        this.Owners[this.back] = this.zone;
-                        this.back += 1;
-                    }
-                    else
-                    {
-                        this.zone += 1;
-                        this.front += 1;
-                        this.mode = Modes.Explore;
-                    }
-                    break;
-            }
-        }
-
-        for (var r = 1; r < rows; r++)
-        {
-            var offset = columns * r;
-            this.back = 0;
-            this.front = 0;
-
-            while (this.back < columns)
-            {
-                switch (this.mode)
+                for (var b = 0; b < 256; b++)
                 {
-                    case Modes.Explore:
-                        var back = tiles[this.back + offset];
-                        var front = tiles[this.front + offset];
-
-                        var eol = this.front >= (columns - 1);
-                        var connected = front.Offset == back.Offset &&
-                            front.IsLevel() && back.IsLevel();
-
-                        if (connected && !eol)
-                        {
-                            this.front += 1;
-                        }
-                        else
-                        {
-                            var zoneBackN = this.Owners[(this.back + offset) - columns];
-                            var zoneFrontN = this.Owners[(this.front + offset) - columns];
-                            this.matchAbove = zoneBackN == zoneFrontN;
-
-
-                            if (this.back > 0)
-                            {
-                                var zoneBackNW = this.Owners[((this.back + offset) - columns) - 1];
-                                this.matchAbove = this.matchAbove && (zoneBackNW != zoneBackN);
-                            }
-                            if (this.front < (columns - 1))
-                            {
-                                var zoneFrontNE = this.Owners[((this.front + offset) - columns) + 1];
-                                this.matchAbove = this.matchAbove && (zoneFrontNE != zoneBackN);
-                            }
-
-                            this.mode = Modes.Catchup;
-                            if (eol) { this.front += 1; }
-                        }
-
-                        break;
-
-                    case Modes.Catchup:
-                        var zone = this.zone;
-                        if (this.matchAbove)
-                        {
-                            zone = this.Owners[(this.back + offset) - columns];
-                        }
-                        if (this.back < this.front)
-                        {
-                            this.Owners[this.back + offset] = zone;
-                            this.back += 1;
-                        }
-                        else
-                        {
-                            this.zone += this.matchAbove ? 0 : 1;
-                            this.front += 1;
-                            this.mode = Modes.Explore;
-                        }
-                        break;
+                    this.AllColors[i++] = new ColorLinear(r / 255.0f, g / 255.0f, b / 255.0f);
                 }
             }
         }
@@ -154,8 +29,8 @@ public sealed class ZoneTerrainColorizer : ITerrainColorizer
 
     public ColorLinear GetColor(IReadOnlyList<Tile> tiles, int i, IReadOnlyList<TerrainVertex> vertices, int a, int b, int c)
     {
-        var owner = this.Owners[i];
-        var index = owner % this.ColorPalette.Colors.Count;
-        return Colors.RGBToLinear(this.ColorPalette.Colors[index]);
+        var owner = this.State.Owners[i];
+        var index = (int)Ranges.Map(owner, (0.0f, this.State.Zone), (0.0f, (256.0f * 256.0f * 256.0f) - 1.0f));
+        return this.AllColors[index];
     }
 }
